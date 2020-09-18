b2dcc44e99f39f94263ccb6e74506bdbe4f4ebe4
samayaramesh <samayaramesh@gmail.com>
2020-09-18 15:38:43 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,10 +2,6 @@ output "instance_ids" {
     value = [aws_instance.web.*.public_ip]
 }
 
-output "pem" {
-        value = ["${tls_private_key.key_name.private_key_pem}"]
-}
-
 output "elb_dns_name" {
   value = "${aws_elb.mw_elb.dns_name}"
 }
