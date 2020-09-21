a2fd977dd9542a1b94eee00f651eb4d4cb11a895
samayaramesh <samayaramesh@gmail.com>
2020-09-21 10:50:14 +0530

    Update asg.tf

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -5,6 +5,25 @@ resource "aws_launch_configuration" "mw_lc" {
   image_id               = var.aws_ami
   instance_type          = var.aws_instance_type
   security_groups        = [aws_security_group.mw_sg_ec2.id]
+  depends_on = [aws_security_group.mw_sg_ec2]
+  subnet_id = element(aws_subnet.mw_app_subnet.*.id, count.index)
+  associate_public_ip_address = true
+  tags = {
+    Name = element(var.aws_webserver, count.index)
+  }
+  provisioner "remote-exec" {
+     inline = ["sudo yum install python -y"]
+        connection {
+                type        = "ssh"
+                private_key = file(var.private_key)
+                user        = var.ansible_user
+                host        = self.public_ip
+  }
+ }
+   provisioner "local-exec" {
+    command = "ansible-playbook site.yml -u var.ansible_user --private-key var.private_key -i tag_Name_web"
+  }
+}
   
   lifecycle {
     create_before_destroy = true
@@ -16,7 +35,7 @@ resource "aws_autoscaling_group" "mw_asg" {
   launch_configuration = aws_launch_configuration.mw_lc.id
  #availability_zones = element(var.azs, count.index)
   availability_zones = data.aws_availability_zones.all.names
-  min_size = 0
+  min_size = 2
   max_size = 10
   load_balancers = [aws_elb.mw_elb.name]
   health_check_type = "ELB"
